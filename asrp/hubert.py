a4d3971fd1a94be8c9da4373ca664b0866bae3fa
voidful <voidful.stack@gmail.com>
2021-08-02 20:41:57 +0800

    reduce Hubert code memory usage

--- asrp/hubert.py
+++ asrp/hubert.py
@@ -8,6 +8,7 @@ class HubertCode(object):
     def __init__(self, hubert_model, km_path, km_layer):
         self.processor = Wav2Vec2FeatureExtractor.from_pretrained(hubert_model)
         self.model = HubertModel.from_pretrained(hubert_model)
+        self.model.eval()
         self.km_model = joblib.load(km_path)
         self.km_layer = km_layer
         self.C_np = self.km_model.cluster_centers_.transpose()
@@ -21,15 +22,16 @@ class HubertCode(object):
             self.model = self.model.cuda()
 
     def __call__(self, filepath, sampling_rate=None):
-        speech, sr = sf.read(filepath)
-        input_values = self.processor(speech, return_tensors="pt", sampling_rate=sr).input_values
-        if torch.cuda.is_available():
-            input_values = input_values.cuda()
-        hidden_states = self.model(input_values, output_hidden_states=True).hidden_states
-        x = hidden_states[self.km_layer].squeeze()
-        dist = (
-                x.pow(2).sum(1, keepdim=True)
-                - 2 * torch.matmul(x, self.C)
-                + self.Cnorm
-        )
-        return dist.argmin(dim=1).cpu().numpy()
+        with torch.no_grad():
+            speech, sr = sf.read(filepath)
+            input_values = self.processor(speech, return_tensors="pt", sampling_rate=sr).input_values
+            if torch.cuda.is_available():
+                input_values = input_values.cuda()
+            hidden_states = self.model(input_values, output_hidden_states=True).hidden_states
+            x = hidden_states[self.km_layer].squeeze()
+            dist = (
+                    x.pow(2).sum(1, keepdim=True)
+                    - 2 * torch.matmul(x, self.C)
+                    + self.Cnorm
+            )
+            return dist.argmin(dim=1).cpu().numpy()
