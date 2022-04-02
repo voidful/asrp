a7440e871877dd5f1af31154a9eca1d6b9ee19ea
voidful <voidful.stack@gmail.com>
2022-04-03 01:54:16 +0800

    improve voice2code quality by merging unit

--- asrp/voice2code.py
+++ asrp/voice2code.py
@@ -1,17 +1,19 @@
 from itertools import groupby
 
 import joblib
+import numpy
 import torch
 import torchaudio
 from transformers import Wav2Vec2FeatureExtractor, HubertModel
 
 
 class HubertCode(object):
-    def __init__(self, hubert_model, km_path, km_layer, return_diff=False, sampling_rate=16000):
+    def __init__(self, hubert_model, km_path, km_layer, return_diff=False, sampling_rate=16000, chunk_sec=5):
         self.processor = Wav2Vec2FeatureExtractor.from_pretrained(hubert_model)
         self.model = HubertModel.from_pretrained(hubert_model)
         self.model.eval()
         self.sampling_rate = sampling_rate
+        self.chunk_length = sampling_rate * chunk_sec
         self.km_model = joblib.load(km_path)
         self.km_layer = km_layer
         self.return_diff = return_diff
@@ -25,7 +27,7 @@ class HubertCode(object):
             self.Cnorm = self.Cnorm.cuda()
             self.model = self.model.cuda()
 
-    def __call__(self, filepath, merge=True):
+    def __call__(self, filepath, merge=True, beamsearch=True, top_k=10, beamsize=200):
         with torch.no_grad():
             speech, sr = torchaudio.load(filepath)
             if sr != self.sampling_rate:
@@ -36,20 +38,46 @@ class HubertCode(object):
             input_values = self.processor(speech, return_tensors="pt", sampling_rate=self.sampling_rate).input_values
             if torch.cuda.is_available():
                 input_values = input_values.cuda()
-            hidden_states = self.model(input_values, output_hidden_states=True).hidden_states
-            x = hidden_states[self.km_layer].squeeze()
+
+            chunks = torch.split(input_values, self.chunk_length, dim=1)
+            for i, chunk in enumerate(chunks):
+                hidden_states = self.model(chunk, output_hidden_states=True).hidden_states
+                feat = hidden_states[self.km_layer].squeeze()
+                if i == 0:
+                    feature = feat
+                else:
+                    feature = torch.cat([feature, feat], dim=0)
+
             dist = torch.sqrt(
-                x.pow(2).sum(1, keepdim=True)
-                - 2 * torch.matmul(x, self.C)
+                feature.pow(2).sum(1, keepdim=True)
+                - 2 * torch.matmul(feature, self.C)
                 + self.Cnorm
             )
-            min_dist = dist.detach().min(dim=1)
+            min_dist = torch.topk(dist.detach(), top_k, dim=-1, largest=False)
+            pred_ind_array = min_dist.indices.cpu().numpy()
+            pred_values_array = min_dist.values.cpu().numpy()
+            code_output = min_dist.indices.T.cpu().numpy()[0]
+
+            if beamsearch:
+                sequences = [[[], 1.0]]
+                for i_row, v_row in zip(pred_ind_array, pred_values_array):
+                    all_candidates = list()
+                    for seq in sequences:
+                        tokens, score = seq
+                        for k, v in zip(i_row, v_row):
+                            norm_len_rate = (len([k for k, _ in groupby(tokens + [k])]) / len(code_output))
+                            norm_dist_rate = (v / numpy.sum(v_row))
+                            candidate = [tokens + [k], score + norm_len_rate * norm_dist_rate]
+                            all_candidates.append(candidate)
+                    ordered = sorted(all_candidates, key=lambda tup: tup[1], reverse=False)
+                    sequences = ordered[:beamsize]
+                code_output = sequences[0][0]
+
             if merge:
-                unitcode = [k for k, _ in groupby(min_dist.indices.cpu().numpy())]
-            else:
-                unitcode = min_dist.indices.cpu().numpy()
+                code_output = [k for k, _ in groupby(code_output)]
+
             if self.return_diff:
-                return unitcode, x.cpu() - torch.index_select(
-                    torch.tensor(self.C_np.transpose()).cpu(), 0, min_dist.indices.cpu())
+                return code_output, feature.cpu() - torch.index_select(torch.tensor(self.C_np.transpose()).cpu(), 0,
+                                                                       min_dist.indices.cpu())
             else:
-                return unitcode
+                return code_output
--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open('requirements.txt') as f:
 
 setup(
     name='asrp',
-    version='0.0.26',
+    version='0.0.27',
     description='',
     url='https://github.com/voidful/asrp',
     author='Voidful',
