f8f234633193c325c124e839ba9689cc4d543083
voidful <voidful.stack@gmail.com>
2023-04-07 00:38:53 +0800

    Update version to 0.0.70 in setup.py

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open('requirements.txt') as f:
 
 setup(
     name='asrp',
-    version='0.0.69',
+    version='0.0.70',
     description='',
     url='https://github.com/voidful/asrp',
     author='Voidful',
